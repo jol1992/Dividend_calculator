e5ca5e05a5dac9a4b921c9e85bfc73d7ef8aa9e2
jol1992 <git@github.com>
2023-08-29 19:56:50 -0500

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { styled } from "styled-components";
+
+const AppWrapper = styled.div`
+  margin: 10px;
+`;
+export const App = () => {
+  return <AppWrapper>Welcome Deshawn</AppWrapper>;
+};
